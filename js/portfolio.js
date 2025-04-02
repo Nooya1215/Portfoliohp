b4266129aed12d163256e5b01e58a16bc6e0e6c4
nooya1215 <nooya1215@gmail.com>
2025-04-02 21:32:18 +0900

    initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".pf-slick").slick({
+      dots: true,
+      speed: 1000,
+      fade: true,
+      arrows: true,
+      autoplay: true,
+      infinite: true,
+      cssEase: 'linear'
+      }
+  );
+});
